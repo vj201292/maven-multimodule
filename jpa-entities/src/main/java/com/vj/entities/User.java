7274a80a6e09fc229dd7308df5a622ba59dd75b5
User <vj.ku.99@gmail.com>
2019-09-17 13:56:05 +0530

    added entity module

---
+++ jpa-entities/src/main/java/com/vj/entities/User.java
@@ -0,0 +1,27 @@
+package com.vj.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+
+}
